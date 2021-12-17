c8177eb6451a3b0a72ac2bb93219fb4a4dcac8ec
farkhan <farhanhamzah71@webmail.umm.ac.id>
2021-12-17 23:57:03 +0700

    install tfjs and tfjs-vis, add model.json and group1-shard1of1.bin

--- src/utils/InitTensorflow.js
+++ src/utils/InitTensorflow.js
@@ -2,3 +2,16 @@
 // 1. Initialize H5 model 
 // 2. Visualize data prediction in graph
 
+import * as tf from "@tensorflow/tfjs";
+import * as tfvis from "@tensorflow/tfjs-vis";
+import modelJson from '../../src/assets/model.json';
+
+async function getModel() {
+    const model = await tf.loadLayersModel(modelJson);
+
+    return model;
+}
+
+
+
+
