b95c79525d31b764c7e9a2d1d25f0b5eb0b046ee
farkhan <farhanhamzah71@webmail.umm.ac.id>
2021-12-21 13:03:24 +0700

    fix global button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Col, Row } from 'antd'
 import {FacebookOutlined, TwitterOutlined, InstagramOutlined} from '@ant-design/icons'
 const rowStyle = {padding :'2em', marginTop: '2em', backgroundColor :'#FF725E', color : 'white'}
+const padStyle = { fontSize: 20, padding: '0 10px' }
 
 const Footer = props => {
     return (
@@ -9,9 +10,9 @@ const Footer = props => {
             <Col style={{ fontSize: 18, fontWeight: 700 }}>COVID-19</Col>
             <Col> © Copyright 2021. All Rights Reserved.</Col>
             <Col>
-                <FacebookOutlined style={{ fontSize: 20, padding: '0 10px' }}/>
-                <InstagramOutlined style={{ fontSize: 20, padding: '0 10px' }}/>
-                <TwitterOutlined style={{ fontSize: 20, padding: '0 10px' }}/>
+                <FacebookOutlined style={padStyle}/>
+                <InstagramOutlined style={padStyle}/>
+                <TwitterOutlined style={padStyle}/>
             </Col>
         </Row>
     );
